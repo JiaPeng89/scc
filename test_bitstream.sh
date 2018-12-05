f338143335e8d6a75037633c46d684f4be95d4c6
家鹏 <libby.hjp@antfin.com>
2018-12-05 20:49:08 +0800

    updatet

---
+++ test_bitstream.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+log="log.txt"
+i=1
+for dir in merge
+do
+    for bin in `ls $dir/*_0.bin`
+    do
+        echo "$i: $bin" >> $log
+        ./TAppDecoder -b $bin -o out.yuv
+        HM=`md5 out.yuv | awk '{print $4}'`
+echo $HM
+        adb push $bin /data/local/hevcd/stream
+        cd ~/Downloads/git/scc_intra_decoder
+        echo "/data/local/hevcd/stream/$bin" > name.txt
+        source test.sh
+        LIBHEVC=`md5 out.yuv | awk '{print $4}'`
+echo $LIBHEVC
+        cd -
+        if [ "$HM"x = "$LIBHEVC"x ];then
+            echo "decode success!" >> $log
+        else
+            echo "decode FAIL" >> $log
+            break
+        fi
+        i=`expr $i + 1`
+    done
+
+    for bin in `ls $dir/*_0.bin`
+    do
+        echo "$i: $bin" >> $log
+        ./TAppDecoder -b $bin -o out.yuv
+        HM=`md5 out.yuv | awk '{print $4}'`
+        echo $HM
+        adb push $bin /data/local/hevcd/stream
+        cd ~/Downloads/git/scc_intra_decoder
+        echo "/data/local/hevcd/stream/$bin" > name.txt
+        source test.sh
+        LIBHEVC=`md5 out.yuv | awk '{print $4}'`
+        echo $LIBHEVC
+        cd -
+        if [ "$HM"x = "$LIBHEVC"x ];then
+            echo "decode success!" >> $log
+        else
+            echo "decode FAIL" >> $log
+            break
+        fi
+        i=`expr $i + 1`
+    done
+done
+
+
+#./TAppEncoder -c encoder_randomaccess_main_scc.cfg -c SlideShow_444.cfg --Log2ParallelMergeLevel= -b ALI_MERGE_G.bin
