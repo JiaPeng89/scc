5b47aae6c93da9c3cff383a86f8c8891a3fcede7
sheldonma <45626996+sheldonma@users.noreply.github.com>
2018-12-06 15:50:32 +0800

    Add files via upload
    修改shell为标准形式

--- run_pmerge.sh
+++ run_pmerge.sh
@@ -3,6 +3,7 @@
 output_pmerge="pmerge.csv"
 echo "FILE,Log2ParallelMergeLevel" > $output_pmerge
 array=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+jarray=(ai ra lb lp)
 #sequence=(SlideShow Console Desktop FlyingGraphics Map Programming Robot WebBrowsing WordEditing)
 i=0
 f=30
@@ -13,28 +14,16 @@ do
 		echo "ALI_PMERGE_${array[$i]} , $Log2ParallelMergeLevel "  >> $output_pmerge
 		for s in SlideShow Console Desktop FlyingGraphics Map Programming Robot WebBrowsing WordEditing
 		do
-#            ./TAppEncoderStatic -c encoder_randomaccess_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=0 --PaletteMode=0 -b ${dir}/ALI_PMERGE_ra_${s}_${array[$i]}_0.bin
-#            ./TAppEncoderStatic -c encoder_randomaccess_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=1 --PaletteMode=0 -b ${dir}/ALI_PMERGE_ra_${s}_${array[$i]}_1.bin
-#            ./TAppEncoderStatic -c encoder_randomaccess_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=0 --PaletteMode=1 -b ${dir}/ALI_PMERGE_ra_${s}_${array[$i]}_2.bin
-#            ./TAppEncoderStatic -c encoder_randomaccess_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=1 --PaletteMode=1 -b ${dir}/ALI_PMERGE_ra_${s}_${array[$i]}_3.bin
-#
-#
-#            ./TAppEncoderStatic -c encoder_lowdelay_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=0 --PaletteMode=0 -b ${dir}/ALI_PMERGE_lb_${s}_${array[$i]}_0.bin
-#            ./TAppEncoderStatic -c encoder_lowdelay_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=1 --PaletteMode=0 -b ${dir}/ALI_PMERGE_lb_${s}_${array[$i]}_1.bin
-#            ./TAppEncoderStatic -c encoder_lowdelay_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=0 --PaletteMode=1 -b ${dir}/ALI_PMERGE_lb_${s}_${array[$i]}_2.bin
-#            ./TAppEncoderStatic -c encoder_lowdelay_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=1 --PaletteMode=1 -b ${dir}/ALI_PMERGE_lb_${s}_${array[$i]}_3.bin
-#
-#
-#            ./TAppEncoderStatic -c encoder_lowdelayp_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=0 --PaletteMode=0 -b ${dir}/ALI_PMERGE_lp_${s}_${array[$i]}_0.bin
-#            ./TAppEncoderStatic -c encoder_lowdelayp_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=1 --PaletteMode=0 -b ${dir}/ALI_PMERGE_lp_${s}_${array[$i]}_1.bin
-#            ./TAppEncoderStatic -c encoder_lowdelayp_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=0 --PaletteMode=1 -b ${dir}/ALI_PMERGE_lp_${s}_${array[$i]}_2.bin
-#            ./TAppEncoderStatic -c encoder_lowdelayp_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=1 --PaletteMode=1 -b ${dir}/ALI_PMERGE_lp_${s}_${array[$i]}_3.bin
-
-
-			./TAppEncoderStatic -c encoder_intra_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=0 --PaletteMode=0 -b ${dir}/ALI_PMERGE_ai_${s}_${array[$i]}_0.bin
-			./TAppEncoderStatic -c encoder_intra_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=1 --PaletteMode=0 -b ${dir}/ALI_PMERGE_ai_${s}_${array[$i]}_1.bin
-			./TAppEncoderStatic -c encoder_intra_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=0 --PaletteMode=1 -b ${dir}/ALI_PMERGE_ai_${s}_${array[$i]}_2.bin
-			./TAppEncoderStatic -c encoder_intra_main_scc.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=1 --PaletteMode=1 -b ${dir}/ALI_PMERGE_ai_${s}_${array[$i]}_3.bin
+			j=0
+                    	#encoder_randomaccess_main_scc encoder_lowdelay_main_scc encoder_lowdelayp_main_scc
+                    	for cfg in encoder_intra_main_scc
+                    	do
+				./TAppEncoderStatic -c $cfg.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=0 --PaletteMode=0 -b ${dir}/ALI_PMERGE_ai_${jarray[$j]}_${s}_${array[$i]}_0.bin
+				./TAppEncoderStatic -c $cfg.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=1 --PaletteMode=0 -b ${dir}/ALI_PMERGE_ai_${jarray[$j]}_${s}_${array[$i]}_1.bin
+				./TAppEncoderStatic -c $cfg.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=0 --PaletteMode=1 -b ${dir}/ALI_PMERGE_ai_${jarray[$j]}_${s}_${array[$i]}_2.bin
+				./TAppEncoderStatic -c $cfg.cfg -c persequence/${s}_444.cfg --FramesToBeEncoded=$f --Log2ParallelMergeLevel=$Log2ParallelMergeLevel --IntraBlockCopyEnabled=1 --PaletteMode=1 -b ${dir}/ALI_PMERGE_ai_${jarray[$j]}_${s}_${array[$i]}_3.bin
+			j=`expr $j + 1`
+			done
 		done
 		i=`expr $i + 1`
 done
