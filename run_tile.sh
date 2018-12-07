8551bfc20242339bf91c08ec306b00ac58715722
sheldonma <45626996+sheldonma@users.noreply.github.com>
2018-12-07 15:08:25 +0800

    Add files via upload

--- run_tile.sh
+++ run_tile.sh
@@ -2,7 +2,7 @@
 
 output_tile="tile.csv"
 echo "FILE,TileUniformSpacing,NumTileColumnsMinus1,NumTileRowsMinus1,TileColumnWidthArray,TileRowHeightArray,LFCrossTileBoundaryFlag" > $output_tile
-array=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+array=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z AA AB AC AD AE AF AG AH AI AJ AK AL AM AN AO AP AQ AR AS AT AU AV AW AX AY AZ BA BB BC BD BE BF BG BH BI BJ BK BL BM BN BO BP BQ BR BS BT BU BV BW BX BY BZ CA CB CC CD CE CF CG CH CI CJ CK CL CM CN CO CP CQ CR CS CT CU CV CW CX CY CZ DA DB DC DD DE DF DG DH DI DJ DK DL DM DN DO DP DQ DR DS DT DU DV DW DX DY DZ)
 jarray=(ai ra lb lp)
 #sequence=(SlideShow Console Desktop FlyingGraphics Map Programming Robot WebBrowsing WordEditing)
 TileColumnWidthArray=(8 4 2)
