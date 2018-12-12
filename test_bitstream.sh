721b5820bf677ac99f1e73d6afd2de077b712ff3
家鹏 <libby.hjp@antfin.com>
2018-12-12 15:53:43 +0800

    update

--- test_bitstream.sh
+++ test_bitstream.sh
@@ -1,49 +1,54 @@
 #!/bin/sh
 
-log="log.txt"
+log="logsliceSegment.txt"
 i=1
-for dir in merge
+target=0
+for dir in sliceSegment
 do
     for bin in `ls $dir/*_1.bin`
     do
-        echo "$i: $bin" >> $log
-        ./TAppDecoder -b $bin -o out.yuv
-        HM=`md5 out.yuv | awk '{print $4}'`
-echo $HM
-        adb push $bin /data/local/hevcd/stream
-        cd ~/Downloads/git/scc_intra_decoder
-        echo "/data/local/hevcd/stream/$bin" > name.txt
-        source test.sh
-        LIBHEVC=`md5 out.yuv | awk '{print $4}'`
-echo $LIBHEVC
-        cd -
-        if [ "$HM"x = "$LIBHEVC"x ];then
-            echo "decode success!" >> $log
-        else
-            echo "decode FAIL" >> $log
-            break
+        if [ $i -gt $target ];then
+            echo "$i: $bin" >> $log
+            ./TAppDecoder -b $bin -o out.yuv
+            HM=`md5 out.yuv | awk '{print $4}'`
+            echo $HM
+            adb push $bin /data/local/hevcd/stream
+            cd ~/Downloads/git/scc_intra_decoder
+            echo "/data/local/hevcd/stream/$bin" > name.txt
+            source test.sh
+            LIBHEVC=`md5 out.yuv | awk '{print $4}'`
+            echo $LIBHEVC
+            cd -
+            if [ "$HM"x = "$LIBHEVC"x ];then
+                echo "decode success!" >> $log
+            else
+                echo "decode FAIL" >> $log
+                break
+            fi
         fi
         i=`expr $i + 1`
     done
 
     for bin in `ls $dir/*_0.bin`
     do
-        echo "$i: $bin" >> $log
-        ./TAppDecoder -b $bin -o out.yuv
-        HM=`md5 out.yuv | awk '{print $4}'`
-        echo $HM
-        adb push $bin /data/local/hevcd/stream
-        cd ~/Downloads/git/scc_intra_decoder
-        echo "/data/local/hevcd/stream/$bin" > name.txt
-        source test.sh
-        LIBHEVC=`md5 out.yuv | awk '{print $4}'`
-        echo $LIBHEVC
-        cd -
-        if [ "$HM"x = "$LIBHEVC"x ];then
-            echo "decode success!" >> $log
-        else
-            echo "decode FAIL" >> $log
-            break
+        if [ $i -gt $target ];then
+            echo "$i: $bin" >> $log
+            ./TAppDecoder -b $bin -o out.yuv
+            HM=`md5 out.yuv | awk '{print $4}'`
+            echo $HM
+            adb push $bin /data/local/hevcd/stream
+            cd ~/Downloads/git/scc_intra_decoder
+            echo "/data/local/hevcd/stream/$bin" > name.txt
+            source test.sh
+            LIBHEVC=`md5 out.yuv | awk '{print $4}'`
+            echo $LIBHEVC
+            cd -
+            if [ "$HM"x = "$LIBHEVC"x ];then
+                echo "decode success!" >> $log
+            else
+                echo "decode FAIL" >> $log
+                break
+            fi
         fi
         i=`expr $i + 1`
     done
